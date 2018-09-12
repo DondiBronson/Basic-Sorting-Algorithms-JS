2ad88210f576fa5824d84bba444e5779ae8079af
BronsonDev <coluckenbach@runbox.com>
2018-09-12 06:35:37 +0200

    QuickSort finished

---
+++ QuickSort/quickSort.md
@@ -0,0 +1,10 @@
+# Quick Sort
+
+Complexity:
+
+- Best Case: n log_2 n
+- Average Case: 2n ln n
+- Worst Case n^2
+
+Less memory usage than MergeSort (no needed Helperlist, in-place)
+QuickSort is unstable (other than MergeSort)
--- QuickSort/quickSort.ts
+++ QuickSort/quickSort.ts
@@ -1,16 +1,10 @@
-/* TODO: 
-    - comment out the whole algorithm
-    - finish readme
-*/
-
-// FIXME: undestand
-
 let numbers: number[] = [2, 6, 34, 3, 1, 8];
 
 console.log();
 console.log("Unsorted Array🛑 :", numbers);
 console.log();
 
+// this method only calculates the bounds and hands the list over to the quickSortMethod
 function sort(list: number[]) {
   const upperBound: number = list.length - 1;
   const lowerBound: number = 0;
@@ -18,21 +12,29 @@ function sort(list: number[]) {
   return list;
 }
 
+// recursive method
 function quickSort(list: number[], lowerBound: number, upperBound: number) {
   let pivot: number;
+
+  // the list will be split here
   let partitionIndex: number;
 
   if (lowerBound < upperBound) {
+    // pivot can be set random but we take the upperBound
     pivot = upperBound;
+
+    // the new partitionIndex is calculated with the current pivot
     partitionIndex = divide(list, lowerBound, upperBound, pivot);
 
-    // sort the left and the right side
+    // sort the left and the right side recursivly
     quickSort(list, lowerBound, partitionIndex - 1);
     quickSort(list, partitionIndex + 1, upperBound);
   }
+  // return the sorted list
   return list;
 }
 
+// divides the given list
 function divide(
   list: number[],
   lowerBound: number,
@@ -44,18 +46,15 @@ function divide(
 
   for (let i = lowerBound; i < upperBound; i++) {
     if (list[i] <= pivotValue) {
-      swap(list, i, partitionIndex);
+      [list[i], list[partitionIndex]] = [list[partitionIndex], list[i]];
       partitionIndex++;
     }
   }
-  swap(list, upperBound, partitionIndex);
+  [list[upperBound], list[partitionIndex]] = [
+    list[partitionIndex],
+    list[upperBound]
+  ];
   return partitionIndex;
-}
-
-function swap(list: number[], i: number, j: number) {
-  const temp: number = list[i];
-  list[i] = list[j];
-  list[j] = temp;
 }
 
 console.log("Sorted Array🍾 :", sort(numbers));
